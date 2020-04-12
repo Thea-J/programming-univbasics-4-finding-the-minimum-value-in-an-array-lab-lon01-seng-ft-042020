9abad5c8333eae6d4b632ca49f063f0e66e88a97
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 21:58:48 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,16 @@
+#Return the integer in the array has the lowest value
 def find_min_value(array)
-  # Add your solution here
+  inspector =0
+  array.length.times {|j|
+   inspector += array[j]  #Inspector needs to be larger than the sum of the array elements
+  }
+  
+ array.length.times {|i|
+   if array[i] < inspector
+     inspector = array[i]
+   end
+ }
+ inspector
 end
+
+
