1f76fd9bca73f397126d34b14605a00af278189c
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 21:52:40 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,12 @@
 def find_min_value(array)
-   inspector = 0
+    array.length.times {|j|
+      sum += array[j]
+    }
+   
  array.length.times do |i|
-   if array[i] < inspector
-     inspector = array[i]
+   if array[i] < sum
+     sum = array[i]
    end
  end
- inspector
+ sum
 end
